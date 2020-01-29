572f3fbf3c9618b5701f3994028f14ea577fe5c3
Expert <expert2006@ukr.net>
2020-01-29 07:02:14 +0300

    Added Gulp and SCSS

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+// Set-ExecutionPolicy Unrestricted -открыть доступ к скриптам
+// Set-ExecutionPolicy Restricted -закрыть доступ к скриптам
+// Set-ExecutionPolicy RemoteSigned -Скрипты, подготовленные на локальном компьютере, можно запускать без ограничений, скрипты, загруженные из Интернета - только при наличии цифровой подписи.
+
+
+const gulp = require("gulp");
+const browserSync = require('browser-sync').create();
+const watch = require("gulp-watch");
+const sass = require("gulp-sass");
+
+// Gulp таск для компиляции SCSS в CSS
+
+gulp.task("scss", function() {
+   return gulp.src("./src/scss/main.scss")
+   .pipe(sass())
+   .pipe(gulp.dest("./src/css/"))
+   .pipe(browserSync.stream()); 
+});
+
+// Gulp таск для поднятия локального сервера
+
+gulp.task("server", function() {
+    browserSync.init({
+        server: {
+            baseDir: "./src/"
+        }
+    });
+});
+
+// Следим за файлами и обновляем браузер
+
+gulp.task("watch", function() {
+    watch(["./src/*.html", "./src/*.js", "./src/img/*.*"], 
+    gulp.parallel(browserSync.reload)
+    );
+
+    watch("./src/scss/**/*.scss", function() {
+        setTimeout(gulp.parallel("scss"),1000);
+    });
+
+});
+
+
+
+// Запускаем дефорлтный таск
+
+gulp.task("default", gulp.series("scss", gulp.parallel("server", "watch")));
